a18a18477a9c26dbb6ae4d4bba7cfdfd234e7079
M Price <zaptross@outlook.com>
2022-10-12 00:15:06 +1000

    feat: install script v2, updated readme

--- README.md
+++ README.md
@@ -5,3 +5,15 @@ Easy install:
 ```bash
 curl -fsSL https://raw.githubusercontent.com/zaptross/custom-zsh/main/install.sh | sh
 ```
+
+### Installed program list
+* [Brave Browser](https://brave.com/linux/)
+* [curl](https://curl.se/)
+* [vim](https://www.vim.org/)
+* [vim Gnome Toolkit 3](https://packages.debian.org/stretch/vim-gtk3)
+* [git](https://git-scm.com/)
+* [xsel](https://linux.die.net/man/1/xsel)
+* [tmux](https://github.com/tmux/tmux/wiki)
+* [flameshot](https://flameshot.org/#download)
+* [oh-my-zsh](https://ohmyz.sh/)
+
--- install.sh
+++ install.sh
@@ -1,76 +1,80 @@
-#!/bin/bash
+zshCustom=~/.oh-my-zsh/custom/custom.zsh
+cloneZsh=~/repos/custom-zsh
+
+installing() {
+	echo "installing $1..."
+}
+
+appendToCustom() {
+	if [ -z $zshCustom ]; then
+		touch $zshCustom
+	fi
+
+	echo "$1" >> $zshCustom
+}
+
+doClone() {
+	git clone git@github.com:Zaptross/$1.git ~/repos/$1
+}
+
+waitForSpace() {
+    read -n1 -s -r -p $'Press space to continue...\n\n' key
+}
 
 sudo apt update
 sudo apt upgrade -y
+sudo apt-get update
 
-curl -fsSL "https://raw.githubusercontent.com/zaptross/custom-zsh/main/.vimrc" > ~/.vimrc
-curl -fsSL "https://raw.githubusercontent.com/zaptross/custom-zsh/main/.tmux.conf" > ~/.tmux.conf
+# install brave browser
+installing brave-browser
+sudo apt install apt-transport-https curl -y
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt install brave-browser -y
 
-if ! command -v zsh; then
-  echo "Installing Zsh..."
-  sudo apt install zsh -y > /dev/null 2>&1
-fi
+# install vim
+installing vim
+sudo apt install vim -y
+sudo apt install vim-gtk3 -y
+export EDITOR=vim
+export VISUAL=vim
 
-if ! command -v git; then
-  echo "Installing git..."
-  sudo apt install git -y > /dev/null 2>&1
-fi
+# install git
+installing git
+sudo apt install git-all -y
 
-if [ ! -d ~/.oh-my-zsh ]; then
-  echo "Installing Oh My Zsh..."
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-fi
+# install xsel
+installing xsel
+sudo apt install xsel -y
 
-appendToCustom() {
-  echo "$1" >> ~/.oh-my-zsh/custom/custom.zsh
-}
+# install tmux
+installing tmux
+sudo apt-get install tmux -y
 
-if [ -z ~/.oh-my-zsh/custom/custom.zsh ]; then
-  touch ~/.oh-my-zsh/custom/custom.zsh
-  echo "Created missing custom.zsh file..."
-fi
+# install flameshot
+installing flameshot
+sudo apt install flameshot -y
 
-if [ -z ~/.oh-my-zsh/custom/oh-my-customizations.zsh ]; then
-  echo "Downloading main customisations file..."
-  curl -fsSL "https://raw.githubusercontent.com/zaptross/custom-zsh/main/oh-my-customizations.zsh" > ~/.oh-my-zsh/custom/oh-my-customizations.zsh
+# create a ssh key and wait for user to install it to github
+echo "\n\nCreating an ssh key for use with git, assuming saved into default file...\n\n"
+ssh-keygen
+cat ~/.ssh/id_rsa.pub | xsel --clipboard
+xdg-open "https://github.com/settings/keys"
+echo "\n\nPlease add the key stored in ~/.ssh/id_rsa.pub (most likely copied to your clipboard) to your github account to allow cloning of repositories.\nWhen complete:"
+waitForSpace
 
-  echo "Creating local customisations file..."
-  echo "# Put customisations for a specific system or environment below" > ~/.oh-my-zsh/custom/oh-my-local.zsh
+mkdir ~/repos
+doClone custom-zsh
 
-  echo "Configuring customizations to load..."
-  appendToCustom "source ~/.oh-my-zsh/custom/oh-my-customizations.zsh"
-  appendToCustom "source ~/.oh-my-zsh/custom/oh-my-local.zsh"
-fi
+# install ohmyzsh
+installing oh-my-zsh
+sudo apt install zsh -y
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-if [ -z /usr/local/bin/direnv ]; then
-  echo "Installing direnv..."
-  bin_path="/usr/local/bin/" curl -sfL "https://direnv.net/install.sh" | bash
-else
-  echo "Found direnv..."
-fi
-
-echo "Configuring customizations to load..."
-appendToCustom "eval \"$(/usr/local/bin/direnv hook zsh)\""
-
-if [ command -v docker ]; then
-  echo "Installing docker..."
-  sudo apt-get update -y
-  sudo apt-get install -y \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-  echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-fi
-
-if ! dpkg -s "xsel" > /dev/null 2>&1; then
-  echo "Installing xsel..."
-  sudo apt-get install xsel
-fi
-
-chsh -s "$(which zsh)" $USER
-
-echo "Completed setup!"
+# setup my customizations
+echo "Setting up customizations..."
+ln "$(echo $cloneZsh)/.vimrc" ~/.vimrc
+ln "$(echo $cloneZsh)/.tmux.conf" ~/.tmux.conf
+appendToCustom "source $(echo $cloneZsh)/oh-my-customizations.zsh"
+echo "# Put customisations for this specific system or environment below" > "$(echo $cloneZsh)/oh-my-local.zsh"
+appendToCustom "source $(echo $cloneZsh)/oh-my-local.zsh"
